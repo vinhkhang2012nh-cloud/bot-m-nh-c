d8dd37a485d4569e1de8155b2191fe5014595a72
vinhkhang2012nh-cloud <vinhkhang2012nh@gmail.com>
2026-09-13 11:18:57 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -63,10 +63,8 @@ ytdl_format_options = {
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
 
 ffmpeg_options = {
-    'before_options': (
-        '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'
-    ),
-    'options': '-vn',
+    'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
+    'options': '-vn -filter:a "atempo=1.0" -ar 48000 -ac 2',
 }
 # ==========================================
 # 4. QUẢN LÝ HÀNG ĐỢI VÀ TRẠNG THÁI NHẠC
